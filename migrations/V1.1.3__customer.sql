cc192a6972ed87e1daa5c7ceba335b4df6a88a5d
Bharath9t7 <39058992+Bharath9t7@users.noreply.github.com>
2022-08-20 14:43:59 +0530

    Create customer table

---
+++ migrations/V1.1.3__customer.sql
@@ -0,0 +1,15 @@
+use schema demo;
+
+create table raw.jaffle_shop.customers 
+( id integer,
+  first_name varchar,
+  last_name varchar
+);
+
+copy into raw.jaffle_shop.customers (id, first_name, last_name)
+from 's3://dbt-tutorial-public/jaffle_shop_customers.csv'
+file_format = (
+  type = 'CSV'
+  field_delimiter = ','
+  skip_header = 1
+  );
